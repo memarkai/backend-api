0e4484fdaa7a37f072421d574fdf0e70812c48bc
Rodrigo César Diniz e Lima <rcdl@cin.ufpe.br>
2019-05-06 02:33:11 -0300

    Add ClinicUser to the Admin site

--- clinics/admin.py
+++ clinics/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from clinics.models import ClinicUser 
+
+
+@admin.register(ClinicUser)
+class ClinicUserAdmin(admin.ModelAdmin):
+	exclude = ('password', )
+	readonly_fields = ('created_at', )
+	filter_horizontal = ('specialties', 'plans')
