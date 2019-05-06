fb8df0bb27c377a086de98435e78f2262d4feb8f
Rodrigo César Diniz e Lima <rcdl@cin.ufpe.br>
2019-05-05 21:08:25 -0300

    Add new environment variables
    Add DEBUG and SECRET KEY to env/dev

--- api/settings.py
+++ api/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6q(#sk))z_y(jpc9pi4u4%$5)p)g_j_b2*7oaha@wy7g3wmtc5'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = []
 
