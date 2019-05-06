5b4a9d5d828c355854e60953f540be9098052771
Rodrigo César Diniz e Lima <rcdl@cin.ufpe.br>
2019-05-06 02:56:19 -0300

    Add PatientUser to patients app

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from accounts.models import BaseProfile
+
+
+class PatientUser(BaseProfile):
+	search_radius = models.PositiveSmallIntegerField(default=10, help_text=_(u'em km.'))
+	insurance = models.ForeignKey('shared.HealthInsurance', on_delete=models.CASCADE, null=True, blank=True)
+
+	class Meta:
+		verbose_name = _(u'Paciente')
+		verbose_name_plural = _(u'Pacientes')
