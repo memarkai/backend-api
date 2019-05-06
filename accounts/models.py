1e819d95ca325a074a500473ea7f32faaadc1e4f
Rodrigo César Diniz e Lima <rcdl@cin.ufpe.br>
2019-05-06 01:56:41 -0300

    Create base profile abstract model

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,8 @@ import uuid
 
 from django.db import models
 from django.conf import settings
+from django.core.validators import RegexValidator
+from django.utils.translation import ugettext_lazy as _
 
 from rest_framework.exceptions import PermissionDenied
 
@@ -42,3 +44,18 @@ class UserAuth(UniqueModel):
 
     def serialize(self):
         return vars(self)
+
+
+class BaseProfile(UserAuth):
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message=_("Formato inválido"))
+
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, null=True)
+    phone = models.CharField(max_length=10, null=True)
+    name = models.CharField(max_length=100, null=True)
+    address = models.CharField(max_length=100, null=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
