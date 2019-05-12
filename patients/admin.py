dee3308794c07a00b23b717a4c601dd5ed476da3
Rodrigo César Diniz e Lima <rcdl@cin.ufpe.br>
2019-05-12 16:37:50 -0300

    Add patient to admin

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from patients.models import PatientUser 
+
+
+@admin.register(PatientUser)
+class PatientUserAdmin(admin.ModelAdmin):
+	exclude = ('password', )
+	readonly_fields = ('created_at', )
