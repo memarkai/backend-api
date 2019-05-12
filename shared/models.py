18d378a3a235f63c8efa510d9f30fdcda3bbc218
Rodrigo César Diniz e Lima <rcdl@cin.ufpe.br>
2019-05-12 16:39:06 -0300

    Update BaseProfile model

--- shared/models.py
+++ shared/models.py
@@ -18,23 +18,22 @@ class BaseProfile(models.Model):
     password = models.CharField(max_length=200)
 
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message=_("Formato inválido"))
-    phone = models.CharField(validators=[phone_regex], max_length=17, null=True)
-    name = models.CharField(max_length=100, null=True)
-    address = models.CharField(max_length=100, null=True)
+    phone = models.CharField(validators=[phone_regex], max_length=17, null=True, blank=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
     created_at = models.DateTimeField(default=timezone.now)
 
     class Meta:
         abstract = True
+
+    def __str__(self):
+        return f'{self.email}'
 
     @staticmethod
     def hash_it(msg):
         digest = hmac.new(str(settings.SECRET_KEY).encode(), msg=str(msg).encode(), digestmod=hashlib.sha256).digest()
         return base64.b64encode(digest).decode()
 
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            self.password = BaseProfile.hash_it(self.password)
-        super(UserAuth, self).save(*args, **kwargs)
 
     @staticmethod
     def authenticate(email, password):
@@ -46,11 +45,14 @@ class BaseProfile(models.Model):
             'exp': datetime.datetime.now() + datetime.timedelta(hours=12),
         }, key=settings.SECRET_KEY, algorithm='HS256')
 
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.password = BaseProfile.hash_it(self.password)
+        super().save(*args, **kwargs)
+
     def serialize(self):
         return vars(self)
 
-    def __str__(self):
-        return self.name
 
 class Specialty(models.Model):
 	id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
