66a48e3fdd33bce57606bd9f4d75006fb59930c9
Rodrigo César Diniz e Lima <rcdl@cin.ufpe.br>
2019-05-12 15:18:49 -0300

    Delete chached migrations from dev + api urls

--- api/urls.py
+++ api/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/clinics/', include('clinics.urls')),
+    path('api/patients/', include('patients.urls')),
 ]
