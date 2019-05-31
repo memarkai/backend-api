43278023eda2f1cf6c2825effcd557a218a94430
Gustavo Souza <gustavo.hlopess@gmail.com>
2019-05-31 14:20:08 -0300

    [N/A] commenting admin URL

--- api/urls.py
+++ api/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('api/', include('shared.urls')),
     path('api/clinics/', include('clinics.urls', namespace='clinics')),
     path('api/patients/', include('patients.urls', namespace='patients')),
