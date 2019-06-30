92b11e0eb9bba26c36e6472a7d9d66358530e2e3
Gustavo Souza <gustavo.hlopess@gmail.com>
2019-06-30 18:27:10 -0300

    list candidates per clinic

--- schedule/views.py
+++ schedule/views.py
@@ -118,5 +118,7 @@ def list_doctor_schedule(request, doctor_id):
 def list_clinic_candidates(request, clinic_id):
     all_consultations = search.list_all_candidates_for_clinic(clinic_id)
     hits = all_consultations['hits']['hits']
-    candidates_ids = [cid for cid in h['_source']['candidates'] for h in hits]
+    candidates_ids = []
+    for h in hits:
+        candidates_ids.extend([cid for cid in h['_source']['candidates']])
     return JsonResponse(candidates_ids, safe=False, status=status.HTTP_200_OK)
